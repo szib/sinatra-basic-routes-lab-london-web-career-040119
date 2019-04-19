9bb8effd070d961873d95ade8254fd1029aefe32
Ivan Szebenszki <szib77@gmail.com>
2019-04-19 17:03:06 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Ivan"
+  end
+  
+  get '/hometown' do
+    "My hometown is Balassagyarmat"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is ソラニン"
+  end
+
 end
